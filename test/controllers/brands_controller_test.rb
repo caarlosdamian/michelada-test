9731ee8d247ac548bf5c8ff262c5e49d2d899e5a
caarlosdamian <dapecarlos@gmail.com>
2017-08-06 20:09:02 -0500

    trying and failing to add tests

--- test/controllers/brands_controller_test.rb
+++ test/controllers/brands_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class BrandsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET index" do
+    get brands_path
+    assert_response :success
+  end
+
+  test "POST create" do
+    new_brand = 'Apple'
+    post brands_path, params: { brand: { name: new_brand } }
+
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET index" do
+    get products_path
+    assert_response :success
+  end
+
+  test "POST create" do
+    new_product = {
+      name: "macbook pro",
+      price: 143,
+      description: "A laptop"
+    }
+    post brands_path, params: { brand: { name: new_brand } }
+
+  end
 end
